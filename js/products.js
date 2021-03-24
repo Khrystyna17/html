5040931d051e71b7b256305a88b4cfa0a69dffb1
Khrystyna Zastavna <khrystynazast@gmail.com>
2021-03-24 17:43:29 +0000

    ajax

--- js/products.js
+++ js/products.js
@@ -1,4 +1,6 @@
-const productsJSON = `
+//const productsJSON = ``;
+
+/*
 [
     {
         "id": "1",
@@ -29,7 +31,38 @@ const productsJSON = `
         "price": 34.99
     }
 ]
-`;
+; */
+
+// function renderProducts(products, sortOrder = 'ascending') {
+//     const productsContainer = document.querySelector('.product-list');
+//     const sortedProducts = products.slice()
+//                             .sort( (a, b) => sortOrder === 'ascending' ? a.price - b.price
+//                                                                            : b.price - a.price );
+//     productsContainer.innerHTML = '';
+//     for (const product of sortedProducts) {
+//         productsContainer.innerHTML += `
+//             <article>
+//                 <h3>${product.title}</h3>
+//                 <img src="img/${product.image}" alt="${product.title}">
+//                 <p>${product.description}</p>
+//                 <div class="button-container">
+//                     <button class="button card-button">Info</button>
+//                     <button class="button card-button">${product.price} - Buy</button>
+//                 </div>
+//             </article>`
+//     }
+// }
+
+
+//renderProducts(JSON.parse(productsJSON));
+
+// function fetchAndRenderProducts(order) {
+//     fetch('products.json')
+//       .then( response => response.json() )
+//       .then( products => renderProducts(products, order) );
+// }
+
+/////////////////////////////////////////////////////////////////////////////////////
 
 function renderProducts(products, sortOrder = 'ascending') {
     const productsContainer = document.querySelector('.product-list');
@@ -51,7 +84,34 @@ function renderProducts(products, sortOrder = 'ascending') {
     }
 }
 
-renderProducts(JSON.parse(productsJSON));
+// function fetchAndRenderProducts(order) {
+//     fetch('products.json')
+//       .then( response => response.json() )
+//       .then( products => renderProducts(products, order) );
+// }
+
+async function fetchAndRenderProducts(order) {
+    const response = await fetch('products.json');
+    const products = await response.json();
+    renderProducts(products, order);
+}
+
+// AJAX sample
+// function getByAjaxAndRenderProducts(order) {
+//     const xhr = new XMLHttpRequest();
+//     xhr.onreadystatechange = function () {
+//         if (xhr.readyState === 4 && xhr.status === 200 ) {
+//             const products = JSON.parse(xhr.responseText);
+//             renderProducts(products, order);
+//         }
+//     }
+//     xhr.open('GET', 'products.json', true);
+//     xhr.send();
+// }
+
+// getByAjaxAndRenderProducts('ascending');
+
+fetchAndRenderProducts('ascending');
 
 const sortAscendingButton = document.querySelector('.sort-asc');
 const sortDescendingButton = document.querySelector('.sort-desc');
@@ -62,11 +122,11 @@ sortDescendingButton.addEventListener('click', sortProductDescending);
 function sortProductAscending() {
     sortDescendingButton.classList.remove('active');
     sortAscendingButton.classList.add('active');
-    renderProducts(JSON.parse(productsJSON), 'ascending');
+    fetchAndRenderProducts('ascending');
 }
 
 function sortProductDescending() {
     sortDescendingButton.classList.add('active');
     sortAscendingButton.classList.remove('active');
-    renderProducts(JSON.parse(productsJSON), 'descending');
+    fetchAndRenderProducts('descending');
 }
